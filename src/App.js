b6dbbf37386e783ff09eb28fb691e63b75bba3b1
hdpuk86 <hayley.d.prior@gmail.com>
2018-05-20 22:04:40 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,66 +1,11 @@
 import React, { Component } from 'react';
-import Header from './containers/global/header-container.js';
-import PageSection from './containers/sections/page-section-container';
-import BoxSection from './containers/sections/box-section-container';
-import TypographyExample from './components/examples/typography';
 
+import Sections from './components/pages/sections';
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <Header />
-        <section className="main-content">
-          <PageSection colour="primary">
-            <React.Fragment>
-              <h1>.page-section-colour_primary</h1>
-              <BoxSection>
-                <TypographyExample />
-              </BoxSection>
-            </React.Fragment>
-          </PageSection>
-          <PageSection colour="white">
-            <React.Fragment>
-              <h1>.page-section-white</h1>
-              <BoxSection>
-                <TypographyExample />
-              </BoxSection>
-              <BoxSection>
-                <TypographyExample />
-              </BoxSection>
-            </React.Fragment>
-          </PageSection>
-          <PageSection colour="secondary">
-            <React.Fragment>
-              <h1>.page-section-colour_secondary</h1>
-              <section className="box-section-inline-container">
-                <h1>.box-section-inline-container</h1>
-                <BoxSection inline>
-                  <TypographyExample />
-                </BoxSection>
-                <BoxSection inline>
-                  <TypographyExample />
-                </BoxSection>
-            </section>
-            </React.Fragment>
-          </PageSection>
-          <PageSection colour="white">
-            <React.Fragment>
-              <h1>.page-section-colour_white</h1>
-              <section className="box-section-inline-container">
-                <h1>.box-section-inline-container</h1>
-                <BoxSection inline>
-                  <TypographyExample />
-                </BoxSection>
-                <BoxSection inline>
-                  <TypographyExample />
-                </BoxSection>
-                <BoxSection inline>
-                  <TypographyExample />
-                </BoxSection>
-            </section>
-            </React.Fragment>
-          </PageSection>
-        </section>
+        <Sections />
       </div>
     );
   }
